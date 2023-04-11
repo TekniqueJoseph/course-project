ed39dd92c7e2be64505d79eb46d3d67fb871b60e
TekniqueJoseph <joejacksun@gmail.com>
2023-04-10 20:02:00 -0400

    added particles and detect box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react'
+import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div className='App'>
+        <ParticlesBg type="cobweb" bg={true} />
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm />
-        <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
       </div>
     )
   }
