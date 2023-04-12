0be0578cd31c159737eb3ac9c0b478c0c69a974e
TekniqueJoseph <joejacksun@gmail.com>
2023-04-12 00:15:03 -0400

    added Face Recognition

--- src/App.js
+++ src/App.js
@@ -56,7 +56,20 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
+    }
+  }
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   }
 
@@ -86,7 +99,7 @@ class App extends Component {
         })
 
     }
-    // this.displayFaceBox(this.calculateFaceLocation(response))
+    this.displayFaceBox(this.calculateFaceLocation(response))
   })
   .catch(err => console.log(err));
 }
